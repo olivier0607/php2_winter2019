7ed1546458c8fbb39e2bc219928195db6f751cee
olivier0607 <47198525+olivier0607@users.noreply.github.com>
2019-03-26 23:06:57 -0400

    lab3

---
+++ lab3/OlivierNgo/config/basic_router.php
@@ -0,0 +1,18 @@
+<?php
+namespace Config\Route;
+
+$arrayURI = explode('/', $_SERVER['REQUEST_URI']);
+
+$method = array_pop($arrayURI) . 'Action';
+
+$class = ucfirst(array_pop($arrayURI));
+
+$fileName = $class . '.php';
+
+require $fileName;
+
+$handler = new $class();
+
+$result = $handler->{$method}();
+
+echo $result;
